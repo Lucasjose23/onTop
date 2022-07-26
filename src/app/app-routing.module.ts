5dc7ece5acf0ebf0b470ec1d8a6b6bd4ba3f7e1b
LucasJose56 <75449550+LucasJose56@users.noreply.github.com>
2022-07-26 20:46:16 -0300

    fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContractsComponent } from './pages/contracts/contracts.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: ContractsComponent },
+  { path: "contract", component: ContractsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { ContractsComponent } from './pages/contracts/contracts.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { ButtonComponent } from './components/button/button.component';
+import { TableComponent } from './components/table/table.component';
+import { FilterComponent } from './components/filter/filter.component';
+import { ActionComponent } from './components/action/action.component';
+import { InputSearchComponent } from './components/input-search/input-search.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContractsComponent,
+    MenuComponent,
+    ButtonComponent,
+    TableComponent,
+    FilterComponent,
+    ActionComponent,
+    InputSearchComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
